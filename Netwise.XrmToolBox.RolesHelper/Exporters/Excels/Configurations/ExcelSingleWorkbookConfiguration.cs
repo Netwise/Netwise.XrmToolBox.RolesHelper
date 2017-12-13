f5edddc3996f87fbc6b86079013c08bc9c9f1d3e
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2017-12-13 15:47:33 +0100

    Finished building header.

--- Netwise.XrmToolBox.RolesHelper/Exporters/Excels/Configurations/ExcelSingleWorkbookConfiguration.cs
+++ Netwise.XrmToolBox.RolesHelper/Exporters/Excels/Configurations/ExcelSingleWorkbookConfiguration.cs
@@ -10,19 +10,41 @@ namespace Netwise.XrmToolBox.RolesHelper.Exporters.Excels.Configurations
         {
             // Worksheet #1 - All
             ExcelWorksheet worksheetAll = package.Workbook.Worksheets.Add("All Roles"); // this[row, column]
-
-            // Entity Permissions title
-            ExcelRange entityPermissionsRange = worksheetAll.Cells["A1:J1"];
-            entityPermissionsRange.Merge = true;
-            entityPermissionsRange.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
-            entityPermissionsRange.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-            entityPermissionsRange.Style.Font.Bold = true;
-            entityPermissionsRange.Style.Font.Color.SetColor(Color.White);
-            entityPermissionsRange.Style.Fill.PatternType = ExcelFillStyle.Solid;
-            entityPermissionsRange.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(47, 86, 131));
-            entityPermissionsRange.Value = "Entity Permissions";
+            this.PrepareHeader(worksheetAll);
 
             // Worksheet #2...n - Worksheet per Role
+        }
+
+        private void PrepareHeader(ExcelWorksheet worksheet)
+        {
+            // Entity Permissions
+            ExcelRange entityPermissionsRange = worksheet.Cells["A1:J1"];
+            this.PrepareHeaderCells(entityPermissionsRange, Color.White, Color.FromArgb(47, 86, 131), "Entity Permissions");
+
+            // Properties columns headers
+            Color ntwColor = Color.FromArgb(166, 206, 57);
+            this.PrepareHeaderCells(worksheet.Cells["A2"], Color.White, ntwColor, "Entity Name");
+            this.PrepareHeaderCells(worksheet.Cells["B2"], Color.White, ntwColor, "Entity Logical Name");
+            this.PrepareHeaderCells(worksheet.Cells["C2"], Color.White, ntwColor, "Role");
+            this.PrepareHeaderCells(worksheet.Cells["D2"], Color.White, ntwColor, "Read");
+            this.PrepareHeaderCells(worksheet.Cells["E2"], Color.White, ntwColor, "Write");
+            this.PrepareHeaderCells(worksheet.Cells["F2"], Color.White, ntwColor, "Delete");
+            this.PrepareHeaderCells(worksheet.Cells["G2"], Color.White, ntwColor, "Append");
+            this.PrepareHeaderCells(worksheet.Cells["H2"], Color.White, ntwColor, "Append To");
+            this.PrepareHeaderCells(worksheet.Cells["I2"], Color.White, ntwColor, "Assign");
+            this.PrepareHeaderCells(worksheet.Cells["J2"], Color.White, ntwColor, "Share");
+        }
+
+        private void PrepareHeaderCells(ExcelRange cells, Color fontColor, Color backgroundColor, string text)
+        {
+            cells.Merge = true;
+            cells.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+            cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            cells.Style.Font.Bold = true;
+            cells.Style.Font.Color.SetColor(fontColor);
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(backgroundColor);
+            cells.Value = text;
         }
     }
 }
