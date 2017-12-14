713361d179ae3cd05b6eea12fd4f1d8d55cd16f4
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2017-12-14 10:54:04 +0100

    Remade filling system; Need to finish adding images to Excel.

--- Netwise.XrmToolBox.RolesHelper/Exporters/Excels/Configurations/ExcelSingleWorkbookConfiguration.cs
+++ Netwise.XrmToolBox.RolesHelper/Exporters/Excels/Configurations/ExcelSingleWorkbookConfiguration.cs
@@ -1,49 +1,113 @@
 ﻿using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using System.Collections.Generic;
 using System.Drawing;
+using WB_Permissions;
 
 namespace Netwise.XrmToolBox.RolesHelper.Exporters.Excels.Configurations
 {
     public class ExcelSingleWorkbookConfiguration : AbstractExporterConfiguration<ExcelPackage, PluginControl>
     {
+        #region Constants
+
+        /// <summary>
+        /// Number of rows reserved for headers.
+        /// </summary>
+        private const int HEADER_ROWS = 2;
+        /// <summary>
+        /// Number of cells per row.
+        /// </summary>
+        private const int CELLS_IN_ROW = 10;
+        /// <summary>
+        /// Default Excel column width.
+        /// </summary>
+        private const double EXCEL_COLUMN_WIDTH = 8.43;
+
+        #endregion
+
         public override void PrepareData(ExcelPackage package, PluginControl dataHolder)
         {
             // Worksheet #1 - All
-            ExcelWorksheet worksheetAll = package.Workbook.Worksheets.Add("All Roles"); // this[row, column]
+            ExcelWorksheet worksheetAll = package.Workbook.Worksheets.Add("All Roles");
             this.PrepareHeader(worksheetAll);
+            this.PrepareDataRows(worksheetAll, dataHolder.wB_Permissions1.RowsMetadata);
 
             // Worksheet #2...n - Worksheet per Role
         }
 
-        private void PrepareHeader(ExcelWorksheet worksheet)
+        private void PrepareDataRows(ExcelWorksheet worksheet, List<DataRowMetadata> rows)
         {
-            // Entity Permissions
-            this.PrepareHeaderCells(worksheet.Cells["A1:J1"], Color.White, Color.FromArgb(47, 86, 131), "Entity Permissions");
-
-            // Properties columns headers
-            Color ntwColor = Color.FromArgb(166, 206, 57);
-            this.PrepareHeaderCells(worksheet.Cells["A2"], Color.White, ntwColor, "Entity Name");
-            this.PrepareHeaderCells(worksheet.Cells["B2"], Color.White, ntwColor, "Entity Logical Name");
-            this.PrepareHeaderCells(worksheet.Cells["C2"], Color.White, ntwColor, "Role");
-            this.PrepareHeaderCells(worksheet.Cells["D2"], Color.White, ntwColor, "Read");
-            this.PrepareHeaderCells(worksheet.Cells["E2"], Color.White, ntwColor, "Write");
-            this.PrepareHeaderCells(worksheet.Cells["F2"], Color.White, ntwColor, "Delete");
-            this.PrepareHeaderCells(worksheet.Cells["G2"], Color.White, ntwColor, "Append");
-            this.PrepareHeaderCells(worksheet.Cells["H2"], Color.White, ntwColor, "Append To");
-            this.PrepareHeaderCells(worksheet.Cells["I2"], Color.White, ntwColor, "Assign");
-            this.PrepareHeaderCells(worksheet.Cells["J2"], Color.White, ntwColor, "Share");
+            for (int i = 0; i < rows.Count; ++i)
+            {
+                DataRowMetadata row = rows[i];
+                DataRow dataRow = row.DataRow;
+                this.PrepareSingleRow(worksheet, i + HEADER_ROWS + 1, Color.Black, Color.Empty, new object[]
+                {
+                    dataRow.EntityName,
+                    dataRow.EntityLogicalName,
+                    dataRow.Role,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+                }, 15);
+            }
         }
 
-        private void PrepareHeaderCells(ExcelRange cells, Color fontColor, Color backgroundColor, string text)
+        private void PrepareHeader(ExcelWorksheet worksheet)
+        {
+            this.PrepareCells(worksheet.Cells["A1:J1"], Color.White, Color.FromArgb(47, 86, 131), "Entity Permissions");
+
+            Color ntwColor = Color.FromArgb(166, 206, 57);
+            this.PrepareSingleRow(worksheet, 2, Color.White, ntwColor, new object[]
+            {
+                "Entity Name", "Entity Logical Name", "Role", "Read", "Write", "Delete", "Append", "Append To", "Assign", "Share"
+            }, 20);
+        }
+
+        private void PrepareSingleRow(ExcelWorksheet worksheet, int row, Color fontColor, Color backgroundColor, object[] values,
+            double columnWidth = EXCEL_COLUMN_WIDTH)
+        {
+            for (int i = 0; i < CELLS_IN_ROW; ++i)
+            {
+                this.PrepareCells(worksheet.Cells[row, i + 1], fontColor, backgroundColor, values[i]);
+                worksheet.Column(i + 1).Width = columnWidth;
+            }
+
+            // For 2 first columns change size
+            for (int i = 0; i < 2; ++i)
+            {
+                worksheet.Column(i + 1).Width = 2 * columnWidth;
+            }
+        }
+
+        private void PrepareCells(ExcelRange cells, Color fontColor, Color backgroundColor, object value,
+            ExcelHorizontalAlignment horizontalAlignment = ExcelHorizontalAlignment.Center,
+            ExcelVerticalAlignment verticalAlignment = ExcelVerticalAlignment.Center,
+            ExcelFillStyle patternType = ExcelFillStyle.Solid)
         {
             cells.Merge = true;
-            cells.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
-            cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            cells.Style.VerticalAlignment = verticalAlignment;
+            cells.Style.HorizontalAlignment = horizontalAlignment;
             cells.Style.Font.Bold = true;
             cells.Style.Font.Color.SetColor(fontColor);
-            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
-            cells.Style.Fill.BackgroundColor.SetColor(backgroundColor);
-            cells.Value = text;
+
+            // Set background color only if specified - it will remove bounding box around cell
+            if (backgroundColor != Color.Empty)
+            {
+                cells.Style.Fill.PatternType = patternType;
+                cells.Style.Fill.BackgroundColor.SetColor(backgroundColor);
+            }
+
+            // For string add text;
+            // For Image try to insert image
+            if (value is string)
+            {
+                cells.Value = value;
+            }
         }
     }
 }
